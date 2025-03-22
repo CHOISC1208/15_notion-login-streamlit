f28dde6b05fc572073741793e5cd791f1e828bfb
CHOI SEONCHEOL <nieve.n.cook1208@gmail.com>
2025-03-23 00:25:04 +0900

    testカセット追加

---
+++ src/data/03_材料明細.py
@@ -0,0 +1,21 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.kintone_fetch_v3 import KintoneDataManager
+
+API_SETTINGS={
+    'base_url':'https://qhlwv3k5c4j1.cybozu.com/k/v1/records.json',
+    'configs':{
+      'config1':{
+          'app_no':'31',
+          'apitoken':os.getenv('33_query'),
+          'query':''
+      }
+    }
+}
+
+manager=KintoneDataManager(API_SETTINGS)
+manager.fetch_data()
+
+result_df=manager.get_dataframe('config1_main')
